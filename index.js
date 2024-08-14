b3eb2dd70b0d5c2813fd96d6abbee594f2bd1ec3
Colappo <123423293+Colappo@users.noreply.github.com>
2024-08-14 16:38:21 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 const yesBtn = document.getElementById("YesBtn")
 const noBtn = document.getElementById("NoBtn")
+const decorate1 = document.getElementById("decorate1")
+const decorate2 = document.getElementById("decorate2")
+const decorate3 = document.getElementById("decorate3")
+
+function randomSize() {
+    return Math.floor(Math.random() * (400 - 100 + 1) - 100);
+}
+
+function randomX() {
+    return Math.floor(Math.random() * (30-(-30)+1) - 30);
+}
+
+function randomY(){
+    return Math.floor(Math.random() * (30-(-30)+1) - 30);
+}
+
+decorate1.style.height = randomSize() + "px"
+decorate1.style.width = decorate1.style.height
+decorate1.style.transform = "translateX(" + randomX() + "vw) " + "translateY(" + randomY() + "vh)"
+decorate2.style.height = randomSize() + "px"
+decorate2.style.width = decorate2.style.height
+decorate2.style.transform = "translateX(" + randomX() + "vw) " + "translateY(" + randomY() + "vh)"
+decorate3.style.height = randomSize() + "px"
+decorate3.style.width = decorate3.style.height
+decorate3.style.transform = "translateX(" + randomX() + "vw) " + "translateY(" + randomY() + "vh)"
 
 function move1() {
     let X = Math.floor(Math.random() * (30-(-30)+1) - 30);
@@ -17,3 +42,4 @@ function move2() {
 
 yesBtn.addEventListener("mousemove", move1)
 noBtn.addEventListener("mousemove", move2)
+
